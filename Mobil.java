d4a1eb13f7ac5a885c0f609d7b1617495dee87e0
melatimawardina <melatimawardina@github.com>
2015-09-11 04:02:57 -0700

    tugas

---
+++ Mobil.java
@@ -0,0 +1,11 @@
+public class Mobil{
+	String merk;
+	String warna;
+	int tahunProduksi;
+	public void printMobil(){
+		System.out.println("Merk: "+ merk);
+		System.out.println("Warna: "+ warna);
+		System.out.println("Tahun: "+ tahunProduksi);
+		
+	}
+}
